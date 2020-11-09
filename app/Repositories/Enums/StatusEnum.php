6bf6946f2355a2adccc9e7e3fc099bd13eb140c5
chunpat <chunpat@163.com>
2020-11-09 11:29:23 +0800

    整理

---
+++ app/Repositories/Enums/StatusEnum.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zzhpeng
+ * Date: 2020/11/7
+ * Time: 09:38
+ */
+
+namespace App\Repositories\Enums;
+
+class StatusEnum
+{
+    const AVAILABLE = 1; //可用
+
+    const DISABLED = 0; //不可用
+
+}
